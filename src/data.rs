1a311c7b24a6b7c8a3ecb3493ad5a57d838136d1
RGBCube <RGBCube@users.noreply.github.com>
2022-12-23 13:45:24 -0500

    Add colors to config

--- src/data.rs
+++ src/data.rs
@@ -1,6 +1,10 @@
-use std::time::Duration;
+use std::{
+    env,
+    time::Duration,
+};
 
 use log::LevelFilter;
+use poise::serenity_prelude::Color;
 use sqlx::{
     sqlite::{
         SqliteConnectOptions,
@@ -12,9 +16,68 @@ use sqlx::{
 
 use crate::types::Error;
 
+fn string_from_env(name: &str) -> String {
+    env::var(name)
+        .expect(format!("Expected the key '{name}' to be set in the environment.").as_str())
+}
+
+fn color_from_env(name: &str) -> Color {
+    let raw = string_from_env(name);
+
+    Color::from(
+        raw.parse::<u32>().expect(
+            format!(
+                "Expected the value for the color key '{name}' to be a valid color representation \
+                 (got '{raw}')."
+            )
+            .as_str(),
+        ),
+    )
+}
+
+#[derive(Debug)]
+pub struct Colors {
+    pub success: Color,
+    pub error: Color,
+    pub warning: Color,
+    pub info: Color,
+}
+
+impl Default for Colors {
+    fn default() -> Self {
+        Self {
+            success: color_from_env("SUCCESS_COLOR"),
+            error: color_from_env("ERROR_COLOR"),
+            warning: color_from_env("WARNING_COLOR"),
+            info: color_from_env("INFO_COLOR"),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Emotes {
+    pub success: String,
+    pub error: String,
+    pub warning: String,
+    pub info: String,
+}
+
+impl Default for Emotes {
+    fn default() -> Self {
+        Self {
+            success: string_from_env("SUCCESS_EMOTE"),
+            error: string_from_env("ERROR_EMOTE"),
+            warning: string_from_env("WARNING_EMOTE"),
+            info: string_from_env("INFO_EMOTE"),
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct Data {
     pub db: SqlitePool,
+    pub colors: Colors,
+    pub emotes: Emotes,
 }
 
 impl Data {
@@ -33,6 +96,10 @@ impl Data {
             .connect_with(db_options.clone())
             .await?;
 
-        Ok(Self { db: db_pool })
+        Ok(Self {
+            db: db_pool,
+            colors: Default::default(),
+            emotes: Default::default(),
+        })
     }
 }
