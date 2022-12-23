3e3d852be7a9526449e27deb60085a16500d6999
RGBCube <RGBCube@users.noreply.github.com>
2022-12-23 13:45:34 -0500

    Use colors in ping

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -12,10 +12,18 @@ use crate::{
 
 #[command(prefix_command, slash_command)]
 pub async fn ping(ctx: Context<'_, Data, Error>) -> Result<(), Error> {
+    let color = ctx.data().colors.info;
+    let emote = &ctx.data().emotes.info;
+
     let before = time::Instant::now();
 
     let message = ctx
-        .send(|builder| builder.embed(|embed| embed.title("Pong!")))
+        .send(|builder| {
+            builder.embed(|embed| {
+                embed.color(color);
+                embed.title(format!("{} Pong!", emote))
+            })
+        })
         .await?;
 
     let after = time::Instant::now();
@@ -23,8 +31,10 @@ pub async fn ping(ctx: Context<'_, Data, Error>) -> Result<(), Error> {
     message
         .edit(ctx, |builder| {
             builder.embed(|embed| {
+                embed.color(color);
                 embed.title(format!(
-                    "Pong! `{}ms`",
+                    "{} Pong! `{}ms`",
+                    emote,
                     after.duration_since(before).as_millis()
                 ))
             })
