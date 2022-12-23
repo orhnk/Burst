5bc6fba93f4d7bbd00ddace7f892b68be1e25961
RGBCube <RGBCube@users.noreply.github.com>
2022-12-23 15:36:12 -0500

    Cleanup error handler

--- src/handlers/on_error.rs
+++ src/handlers/on_error.rs
@@ -1,6 +1,7 @@
 use log::error;
 use poise::{
     BoxFuture,
+    Context,
     FrameworkError,
 };
 
@@ -9,33 +10,59 @@ use crate::{
     types::Error,
 };
 
-#[allow(unused_must_use, clippy::match_single_binding)] // TODO: Remove this when the error handler is implemented.
-async fn handle(error: FrameworkError<'_, Data, Error>) {
-    if let Some(ctx) = error.ctx() {
-        match error {
-            _ => {
-                error!(
-                    "An error has occured while executing the command {}: {error:?}",
-                    ctx.command().qualified_name
-                );
+#[allow(unused_must_use)]
+async fn uncaught_error(ctx: Context<'_, Data, Error>, error: FrameworkError<'_, Data, Error>) {
+    let data = ctx.data();
 
-                ctx.send(|builder| {
-                    builder.reply(true);
-                    builder.embed(|embed| {
-                        embed.color(ctx.data().colors.error);
-                        embed.title(format!(
-                            "{} An unknown error has occured.",
-                            ctx.data().emotes.error
-                        ));
-                        embed.description("This incident will be reported.")
-                    })
-                })
-                .await;
-            },
-        }
-    }
-    else {
+    error!(
+        "An uncaught error has occured while executing the command {}: {error:?}",
+        ctx.command().qualified_name
+    );
+
+    ctx.send(|builder| {
+        builder.reply(true);
+        builder.embed(|embed| {
+            embed.color(data.colors.error);
+            embed.title(format!(
+                "{} An unknown error has occured.",
+                data.emotes.error
+            ));
+            embed.description("This incident will be reported.")
+        })
+    })
+    .await;
+}
+
+#[allow(unused_must_use)]
+async fn handle(error: FrameworkError<'_, Data, Error>) {
+    let ctx = error.ctx();
+
+    if ctx.is_none() {
         error!("An uncaught error has occured: {error:?}");
+        return;
+    }
+
+    let ctx = ctx.unwrap();
+
+    match error {
+        FrameworkError::ArgumentParse { input, .. } => {
+            ctx.send(|builder| {
+                builder.reply(true);
+                builder.embed(|embed| {
+                    embed.color(ctx.data().colors.error);
+                    embed.title(format!(
+                        "{} Invalid arguments provided.",
+                        ctx.data().emotes.error
+                    ));
+                    if let Some(input) = input {
+                        embed.description(format!("Invalid argument: {}", input));
+                    }
+                    embed
+                })
+            })
+            .await;
+        },
+        _ => uncaught_error(ctx, error).await,
     }
 }
 
